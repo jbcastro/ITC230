6fd1c73475cf08a37ebb59413f28d771caabaa48
Joseph Castro <josephbeckcastro@gmail.com>
2018-07-10 17:31:13 +0000

    Let's get modular completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var http = require("http"); 
       var fs = require("fs");
+      var episodes = require("./lib/episodes.js");
+      var url = require('url');
+      
 http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
+  //var path = req.url.toLowerCase();
+  var path = url.parse(req.url).pathname;
+  
+
+  
   switch(path) {
     case '/':
  
@@ -13,10 +20,32 @@ fs.readFile('public/home.html', function (err, data) {
 });
       break;
       
-    case '/about':
+    case '/get':
+      var thisquery=url.parse(req.url, true).query;
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
+      
+      if(episodes.getOne(thisquery.epnum)){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(JSON.stringify(episodes.getOne(thisquery.epnum)));
+      } else{ 
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify({wat:'Not an episode, or you deleted it.'}));
+        res.end();
+      }
+      res.end();
       break;
+      
+      
+       case '/kill':
+      var thisquery=url.parse(req.url, true).query;
+      
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(JSON.stringify(episodes.killOne(thisquery.epnum)));
+      res.end();
+      
+      break;
+      
+      
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not found');
